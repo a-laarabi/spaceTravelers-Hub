5d0432bebb0ed6b18c53843c6a724dfd2aa873cb
Lusindiso Ntanjana <31928865+Lusindiso@users.noreply.github.com>
2022-08-30 16:45:32 +0200

    Console log rockets

--- src/components/pages/Rockets.js
+++ src/components/pages/Rockets.js
@@ -1,4 +1,17 @@
-const Rockets = () => (
-  <div>Rockets</div>
-);
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import fetchRocketsData from '../../redux/rockets/rocketsActions';
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRocketsData());
+  }, [dispatch]);
+  const rockets = useSelector((state) => state.rockets);
+  console.log(rockets);
+  return (
+    <div>Rockets</div>
+  );
+};
+
 export default Rockets;
