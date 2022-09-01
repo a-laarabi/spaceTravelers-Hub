3323103b1a132aa3f8bc96f429ae795bb2613ca6
Lusindiso Ntanjana <31928865+Lusindiso@users.noreply.github.com>
2022-09-01 09:49:55 +0200

    Render rockets

--- src/components/pages/Rockets.js
+++ src/components/pages/Rockets.js
@@ -1,16 +1,25 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import fetchRocketsData from '../../redux/rockets/rocketsActions';
+import Rocket from '../Rocket';
 
 const Rockets = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchRocketsData());
   }, [dispatch]);
-  const rockets = useSelector((state) => state.rockets);
-  console.log(rockets);
+  const rockets = useSelector((state) => state.rockets.rockets);
   return (
-    <div>Rockets</div>
+    <main>
+      {rockets.map((rocket) => (
+        <Rocket
+          key={rocket.id}
+          name={rocket.name}
+          description={rocket.description}
+          src={rocket.image[0]}
+        />
+      ))}
+    </main>
   );
 };
 
