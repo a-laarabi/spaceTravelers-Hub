193722dc8170864f59bf0a7906c2e3d88d772451
anasse Laarabi <contact.laarabi@gmail.com>
2022-09-02 07:38:19 +0100

    adding functions to add and leave an event

--- src/redux/missions/mission.js
+++ src/redux/missions/mission.js
@@ -1,4 +1,6 @@
 const FETCH_MISSION = 'spacetravelers/missions/FETCH_MISSION';
+const JOIN_MISSION = 'spacetravelers/missions/JOIN_MISSION';
+const LEAVE_MISSION = 'spacetravelers/missions/LEAVE_MISSION';
 
 const initialState = [];
 
@@ -6,17 +8,41 @@ const missionReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_MISSION:
       return action.mission;
+    case JOIN_MISSION:
+      return state.map((mission) => {
+        if (mission.id !== action.payload) {
+          return mission;
+        }
+        return { ...mission, reserved: true };
+      });
+    case LEAVE_MISSION:
+      return state.map((mission) => {
+        if (mission.id !== action.payload) {
+          return mission;
+        }
+        return { ...mission, reserved: false };
+      });
     default:
       return state;
   }
 };
 
-export const fetchMission = (mission) => ({
+const fetchMission = (mission) => ({
   type: FETCH_MISSION,
   mission,
 });
 
-export const LoadMissions = () => async (dispatch) => {
+const JoinMissions = (id) => ({
+  type: JOIN_MISSION,
+  payload: id,
+});
+
+const LeaveMission = (id) => ({
+  type: LEAVE_MISSION,
+  payload: id,
+});
+
+const LoadMissions = () => async (dispatch) => {
   const response = await fetch('https://api.spacexdata.com/v3/missions');
   const data = await response.json();
   const missionsFetched = data.map((mission) => ({
@@ -26,6 +52,10 @@ export const LoadMissions = () => async (dispatch) => {
     reserved: false,
   }));
   dispatch(fetchMission(missionsFetched));
+};
+
+export {
+  LoadMissions, fetchMission, JoinMissions, LeaveMission,
 };
 
 export default missionReducer;
