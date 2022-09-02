6fffd7d3aaf24c555f07cbfd3173e04df58ce4fb
anasse Laarabi <contact.laarabi@gmail.com>
2022-09-02 07:51:05 +0100

    update profile

--- src/components/pages/MyProfile.js
+++ src/components/pages/MyProfile.js
@@ -1,4 +1,17 @@
-const MyProfile = () => (
-  <div>MyProfile</div>
-);
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const MyProfile = () => {
+  const missions = useSelector((state) => state.missions);
+  const joinedMissions = missions.filter((mission) => mission.reserved);
+
+  return (
+    <>
+      <h1>My Missions</h1>
+      <ul>
+        {joinedMissions.map((mission) => <li key={mission.id}>{mission.name}</li>)}
+      </ul>
+    </>
+  );
+};
 export default MyProfile;
