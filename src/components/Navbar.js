bc2047c2e82eb613ffda0761480a61d815b99f20
a-laarabi <contact.laarabi@gmail.com>
2022-09-03 01:53:17 +0100

    fixing ESLint

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ const Navbar = () => (
   <nav className="navbar">
     <ul className="nav-links">
       <li key={1}><NavLink exact activeClassName="active" to="./">Rockets</NavLink></li>
-      <li key={3}><NavLink ctiveClassName="active" to="/Mission">Mission</NavLink></li>
+      <li key={2}><NavLink ctiveClassName="active" to="/Mission">Mission</NavLink></li>
       <li>|</li>
-      <li key={4}><NavLink ctiveClassName="active" to="/my-profile">My_Profile</NavLink></li>
+      <li key={3}><NavLink ctiveClassName="active" to="/my-profile">My_Profile</NavLink></li>
     </ul>
   </nav>
 );
