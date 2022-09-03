13993edde01d01f98a51bf130a1e13596f44875e
Lusindiso Ntanjana <31928865+Lusindiso@users.noreply.github.com>
2022-09-03 13:04:07 +0200

    remove keys

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => (
   <nav className="navbar">
     <ul className="nav-links">
-      <li key={1}><NavLink exact activeClassName="active" to="./">Rockets</NavLink></li>
-      <li key={2}><NavLink ctiveClassName="active" to="/Mission">Mission</NavLink></li>
+      <li><NavLink to="./">Rockets</NavLink></li>
+      <li><NavLink to="/Mission">Mission</NavLink></li>
       <li>|</li>
-      <li key={3}><NavLink ctiveClassName="active" to="/my-profile">My_Profile</NavLink></li>
+      <li><NavLink to="/my-profile">My_Profile</NavLink></li>
     </ul>
   </nav>
 );
